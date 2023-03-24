aac8fb6e3d05265a86fc443a65c540e36fac81e2
AmanNainwaya <86603628+AmanNainwaya@users.noreply.github.com>
2023-03-24 21:17:28 +0530

    Update BookController.java

--- src/main/java/com/example/goodreads/BookController.java
+++ src/main/java/com/example/goodreads/BookController.java
@@ -2,22 +2,36 @@ package com.example.goodreads;
 import com.example.goodreads.BookService;
 
 import java.util.*;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 public class BookController{
     BookService bookService = new BookService();
 
+    @DeleteMapping("/books/{bookId}")
+    public void deleteBook(@PathVariable("bookId") int bookId){
+        bookService.deleteBook(bookId);
+    }
+    
+    @PutMapping("/books/{bookId}")
+    public Book updateBook(@PathVariable("bookId") int bookId, @RequestBody Book book) {
+        return bookService.updateBook(bookId, book);
+    }
+
+    @PostMapping("/books")
+    public Book addBook(@RequestBody Book book) {
+        return bookService.addBook(book);
+    }
+
     @GetMapping("/books")
-    public ArrayList<Book> getBooks(){
+    public ArrayList<Book> getBooks() {
         return bookService.getBooks();
     }
 
     @GetMapping("/books/{bookId}")
-    public Book getBookById(@PathVariable("bookId") int bookId){
+    public Book getBookById(@PathVariable("bookId") int bookId) {
         return bookService.getBookById(bookId);
     }
 }
